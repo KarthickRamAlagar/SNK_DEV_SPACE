2be0b10751df8d0c845a0bb5b659f9983e31c01d
karthickramalagare <karthickramalagr@gmail.com>
2026-02-10 10:17:43 +0530

    Fix import path casing for Home component in router.jsx

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,9 @@
 // src/router.jsx
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "../layouts/AppLayout";
-import Home from "../Pages/Home";
+import Home from "../pages/Home";
 import ProtectedRoutes from "../Components/ProtectedRoutes";
-import NotFound from "../Pages/NotFound";
+import NotFound from "../pages/NotFound";
 import ExperienceSection from "@/Components/Experience/ExperienceSection";
 import Contact from "@/Components/Contact/Contact";
 import AboutSection from "@/components/About/AboutSection";
